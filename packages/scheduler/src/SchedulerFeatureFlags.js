b928d954b0bd9e63432a888e0e71bffb7278d1fb
qiufeihong2018 <15058301288@163.com>
2024-06-12 12:52:42 +0800

    docs: update

--- packages/scheduler/src/SchedulerFeatureFlags.js
+++ packages/scheduler/src/SchedulerFeatureFlags.js
@@ -1,16 +1,41 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow strict
+ * 设置启用调度器调试的标志。
+ * 调用者可以根据这个标志来决定是否开启调度器的调试功能。
+ * @type {boolean}
  */
-
 export const enableSchedulerDebugging = false;
+
+/**
+ * 设置启用性能分析的标志。
+ * 当这个标志为真时，将启用额外的性能分析逻辑。
+ * @type {boolean}
+ */
 export const enableProfiling = false;
+
+/**
+ * 定义每一帧的yield时间（以毫秒为单位）。
+ * 这个值决定了在执行一帧的JavaScript代码之前，应该让出多少时间给其他任务。
+ * @type {number}
+ */
 export const frameYieldMs = 5;
 
+/**
+ * 定义用户阻塞优先级任务的超时时间（以毫秒为单位）。
+ * 超时后，这些任务将降级为正常优先级。
+ * @type {number}
+ */
 export const userBlockingPriorityTimeout = 250;
+
+/**
+ * 定义正常优先级任务的超时时间（以毫秒为单位）。
+ * 超时后，这些任务将降级为低优先级。
+ * @type {number}
+ */
 export const normalPriorityTimeout = 5000;
+
+/**
+ * 定义低优先级任务的超时时间（以毫秒为单位）。
+ * 这个超时时间用于控制低优先级任务的最长等待时间。
+ * @type {number}
+ */
 export const lowPriorityTimeout = 10000;
--- packages/scheduler/src/SchedulerPriorities.js
+++ packages/scheduler/src/SchedulerPriorities.js
@@ -1,18 +1,24 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow strict
+ * 定义了任务的优先级级别。
+ * 
+ * 这些常量用于标识不同类型的任务的优先级。优先级越低，任务越不紧急，
+ * 会在其他更高优先级的任务之后执行。优先级的设置有助于调度系统合理安排任务执行的顺序，
+ * 以确保高优先级的任务能够得到及时处理。
+ * 
+ * @enum {number}
  */
-
 export type PriorityLevel = 0 | 1 | 2 | 3 | 4 | 5;
 
+// 无优先级，用于表示一个没有特定优先级的任务。
 // TODO: Use symbols?
 export const NoPriority = 0;
+// 立即执行的优先级，用于非常紧急的任务。
 export const ImmediatePriority = 1;
+// 用户阻塞优先级，用于需要立即响应用户操作的任务。
 export const UserBlockingPriority = 2;
+// 正常优先级，用于大多数常规任务。
 export const NormalPriority = 3;
+// 低优先级，用于可以稍后执行而不影响用户体验的任务。
 export const LowPriority = 4;
+// 闲置优先级，用于在系统处于闲置状态时执行的任务。
 export const IdlePriority = 5;
